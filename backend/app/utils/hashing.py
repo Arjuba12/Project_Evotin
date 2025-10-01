be19265bb2b998565f675189e04fdcee1ae3fe39
Arjuba12 <144748979+Arjuba12@users.noreply.github.com>
2025-10-02 01:36:24 +0700

    Update hashing.py

--- backend/app/utils/hashing.py
+++ backend/app/utils/hashing.py
@@ -2,16 +2,11 @@ from passlib.context import CryptContext
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-def hash_password(password: str) -> str:
-    """Hash password max 72 bytes (bcrypt)"""
-    # potong sesuai bytes
-    max_bytes = 72
-    password_bytes = password.encode("utf-8")[:max_bytes]
-    password_truncated = password_bytes.decode("utf-8", "ignore")
+def hash_password(password: str):
+    # truncate maksimal 72 karakter
+    password_truncated = password[:72]
     return pwd_context.hash(password_truncated)
 
-
-def verify_password(plain_password: str, hashed_password: str) -> bool:
-    """Verify plain password dengan hashed password"""
-    # Tidak perlu dipotong, passlib akan handle verifikasi
-    return pwd_context.verify(plain_password, hashed_password)
+def verify_password(plain_password, hashed_password):
+    plain_truncated = plain_password[:72]
+    return pwd_context.verify(plain_truncated, hashed_password)
