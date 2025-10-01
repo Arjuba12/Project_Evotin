39685288b0630cec803f24b1fbd96c6fb8ca796f
Arjuba12 <144748979+Arjuba12@users.noreply.github.com>
2025-10-01 23:06:07 +0700

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "dist",
-  },
-  base: "./", // <== penting supaya file JS/CSS relatif
-});
+  server: {
+    allowedHosts: [
+      'localhost',
+      '127.0.0.1',
+      'c66c3433-407a-492e-a2ad-20c4b85d51b2-00-1h32aauoe6m8i.sisko.replit.dev'
+    ]
+  }
+})
