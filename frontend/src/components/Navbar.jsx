364be17c631f364e513fd43e8b65be4f5bf942b6
Arjuna Bimantara <arjunabimantara912GMAIL.COM>
2025-10-03 13:22:23 +0700

    Fix Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -37,8 +37,22 @@ export default function Navbar() {
 
         {/* hamburger di kanan (hanya muncul di mobile via CSS) */}
         <div className="menu-icon" onClick={() => setIsOpen(true)}>
-          <span>☰</span> {/* WRAP IKON TEKS DI DALAM SPAN */}
-        </div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            width="32"
+            height="32"
+          >
+            <line x1="3" y1="12" x2="21" y2="12"></line>
+            <line x1="3" y1="6" x2="21" y2="6"></line>
+            <line x1="3" y1="18" x2="21" y2="18"></line>
+          </svg>{" "}
+        </div>
       </nav>
 
       {/* overlay */}
